6d0b5e6a74d5ccf95d0b0469de2a38cb9376ef88
mohamedabdulle <25498272+mohamedabdulle@users.noreply.github.com>
2023-08-20 23:43:29 -0400

    Complete the game

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# tic-tac-toe
+# Tic Tac Toe
+
+This is a 2-player Tic Tac Toe game.
+- The board is a 3 X 3 grid.
+- There is a scoreboard that records the scores, including draws.
+- There are two reset buttons.
+  - The left button resets the board, while the right button resets the scores.
+
+### Full View:
+
+![Tic Tac Toe](docs/tic-tac-toe.png)
+
+### Mobile View:
+
+![Tic Tac Toe Mobile](docs/tic-tac-toe-mobile.png)
+
+## Links
+- [Click here to see the Tic Tac Toe game.](https://mohamedabdulle.github.io/tic-tac-toe/)
+
+- [Link to the assignment details.](https://www.theodinproject.com/lessons/node-path-javascript-tic-tac-toe)
+
+## Description
+
+The purpose of this project was to practise the use of:
+- Factory functions
+- Module pattern
+- Scopes
+- Closures
+- Private variables and functions 
--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const playerOneInput = document.querySelector("#player-1-input");
 const playerOneScore = document.querySelector("#player-1-score");
 const playerTwoInput = document.querySelector("#player-2-input");
 const playerTwoScore = document.querySelector("#player-2-score");
+const resetBoardButton = document.querySelector("#reset-board");
+const resetScoreButton = document.querySelector("#reset-score");
+const drawScore = document.querySelector("#draw-score");
 
 const Player = (playerName, score, playerNameInput, playerScore) => {
   const getScore = () => score;
@@ -10,32 +13,26 @@ const Player = (playerName, score, playerNameInput, playerScore) => {
   const incrementScore = () => playerScore.textContent = ++score;
   const setPlayerName = (newName) => playerName = newName;
   const getPlayerInput = () => playerNameInput;
-
-  return { setPlayerName, getPlayerName, getScore, incrementScore, getPlayerInput };
+  const getPlayerScore = () => playerScore;
+  return { setPlayerName, getPlayerName, getScore, incrementScore, getPlayerInput, getPlayerScore };
 }
 
 const PlayerFactory = (playerName, playerInput, playerScore) => {
-  // const playerName = playerName;
-  // const playerInput = playerInput;
-  // const playerScore = playerScore;
   let score = 0;
-  const { setPlayerName, getPlayerName, getScore, incrementScore, getPlayerInput } = Player(playerName, score, playerInput, playerScore);
-
-  return { setPlayerName, getPlayerName, getScore, incrementScore, getPlayerInput };
+  const { setPlayerName, getPlayerName, getScore, incrementScore, getPlayerInput, getPlayerScore } = Player(playerName, score, playerInput, playerScore);
+  return { setPlayerName, getPlayerName, getScore, incrementScore, getPlayerInput, getPlayerScore };
 }
 
 let playerOne = PlayerFactory("Player 1", playerOneInput, playerOneScore);
 let playerTwo = PlayerFactory("Player 2", playerTwoInput, playerTwoScore);
 
-const gameGrid = ((playerOne, playerTwo) => {
+const gameGrid = ((playerOne, playerTwo, drawScore) => {
 
-  const gridArray = Array(10);
-  const nextWinningMove = new Map();
-
+  let gridArray = Array(10);
+  let nextWinningMove = new Map();
   const GRID_SIZE = 3;
   const crossClass = document.querySelector("#player-1-mark");
   const circleClass = document.querySelector("#player-2-mark");
-  const drawScore = document.querySelector("#draw-score");
   let playerOneTurn = true;
   const CROSS = "X";
   const CIRCLE = "O";
@@ -44,16 +41,10 @@ const gameGrid = ((playerOne, playerTwo) => {
   let countingMoves = 0;
 
   const nextTurn = () => playerOneTurn = !playerOneTurn;
-
-  const addToGridArray = (index) => {
-    (playerOneTurn) ? gridArray[index] = CROSS : gridArray[index] = CIRCLE;
-  }
-
-  const setPlayerMove = (index, grid) => {
-    return (gridArray[index] == CROSS) ?  grid.classList.add(crossClass.className) : grid.classList.add(circleClass.className);
-  }
-
-  const setNextWinningMove = (index, playerSymbol) => {
+  const addToGridArray = (index) => (playerOneTurn) ? gridArray[index] = CROSS : gridArray[index] = CIRCLE;
+  const setPlayerMove = (index, grid) => (gridArray[index] == CROSS) ?  grid.classList.add(crossClass.className) : grid.classList.add(circleClass.className);
+  
+  const setNextWinningMoveMap = (index, playerSymbol) => {
     const winningMove = nextWinningMove.get(index);
 
     if (winningMove == undefined)  {
@@ -66,8 +57,21 @@ const gameGrid = ((playerOne, playerTwo) => {
 
   const isNextWinningMove = (index, playerSymbol) => {
     let winningMove = nextWinningMove.get(index);
-    return (winningMove != undefined) ? winningMove.includes(playerSymbol) : false;
-  }
+    if (winningMove != undefined && winningMove.includes(playerSymbol)) {
+      gameOver = true;
+      
+      let winner = players.get(playerSymbol);
+      winner.incrementScore();
+      (winner.getPlayerScore()).classList.add((playerSymbol === CROSS) ? "player-1-color" : "player-2-color");
+
+      const grid = document.querySelector(`.grid-container > div[data-grid="${index}"]`);
+      grid.classList.add("winning-grid");
+    } else if (countingMoves === GRID_SIZE ** 2) {
+      let score = +drawScore.textContent;
+      drawScore.textContent = ++score;
+      gameOver = true;
+    }
+  } 
 
   const findWinningMove = (index, playerSymbol) => {
     const arg = [index, playerSymbol];
@@ -77,17 +81,21 @@ const gameGrid = ((playerOne, playerTwo) => {
   }
 
   const rowCheck = (index, playerSymbol) => {
+    //Figure out which row to travel through, based on the grid,
+    //to find the next winning move.
     const rowIndex = (Math.ceil(index / GRID_SIZE) * (GRID_SIZE) - (GRID_SIZE - 1));
-    setWinningMove(playerSymbol, rowIndex, 1);
+    setNextWinningMove(playerSymbol, rowIndex, 1);
   }
 
   const columnCheck = (index, playerSymbol) => {
+    //Figure out which column to travel through, based on the grid,
+    //to find the next winning move.
     let columnIndex = index % GRID_SIZE;
 
     // Column 3 remainder is 0.
     // Overwrite to 3 to use it as an index for column
     if (columnIndex === 0) columnIndex = 3;
-    setWinningMove(playerSymbol, columnIndex, GRID_SIZE);
+    setNextWinningMove(playerSymbol, columnIndex, GRID_SIZE);
   }
 
   const diagonalCheck = (index, playerSymbol) => {
@@ -95,27 +103,27 @@ const gameGrid = ((playerOne, playerTwo) => {
     const isLeftDiagonalCheck = ((index - 1) % (GRID_SIZE - 1)) === 0;
     const diagonalCheck = isRightDiagonalCheck && isLeftDiagonalCheck;
 
-    if (isRightDiagonalCheck) leftDiagonalCheck(playerSymbol);
-    else if (isLeftDiagonalCheck) rightDiagonalCheck(playerSymbol);
-    else if (diagonalCheck) {
+    if (diagonalCheck) {
       leftDiagonalCheck(playerSymbol);
       rightDiagonalCheck(playerSymbol);
     }
-  }
-
-  const leftDiagonalCheck = (playerSymbol) => {
-    const startIndex = 1;
-    const increment = GRID_SIZE + 1;
-    setWinningMove(playerSymbol, startIndex, increment);
+    else if (isRightDiagonalCheck) rightDiagonalCheck(playerSymbol);
+    else if (isLeftDiagonalCheck) leftDiagonalCheck(playerSymbol);
   }
 
   const rightDiagonalCheck = (playerSymbol) => {
-    const startIndex = GRID_SIZE;
-    const increment = GRID_SIZE - 1;
-    setWinningMove(playerSymbol, startIndex, increment);
+    const startIndex = 1;
+    const increment = GRID_SIZE + 1;
+    setNextWinningMove(playerSymbol, startIndex, increment);
   }
 
-  const setWinningMove = (playerSymbol, index, increment) => {
+  const leftDiagonalCheck = (playerSymbol) => {
+    const startIndex = GRID_SIZE;
+    const increment = GRID_SIZE - 1;
+    setNextWinningMove(playerSymbol, startIndex, increment);
+  }
+
+  const setNextWinningMove = (playerSymbol, index, increment) => {
     let counter = 0;
     let emptyGridIndex = 0;
     let indexCheck = index;
@@ -126,7 +134,7 @@ const gameGrid = ((playerOne, playerTwo) => {
       if (gridSymbol === playerSymbol) ++counter;
       else if (gridSymbol == undefined) emptyGridIndex = indexCheck;
 
-      if (counter === 2 && emptyGridIndex != 0) setNextWinningMove(emptyGridIndex, playerSymbol);
+      if (counter === 2 && emptyGridIndex != 0) setNextWinningMoveMap(emptyGridIndex, playerSymbol);
       indexCheck = indexCheck + increment;
     }
   }
@@ -136,24 +144,36 @@ const gameGrid = ((playerOne, playerTwo) => {
     countingMoves++;
     addToGridArray(index);
     const playerSymbol = gridArray[index];
-
     setPlayerMove(index, grid);
-    if (isNextWinningMove(index, playerSymbol)) {
-      gameOver = true;
-      let winner = players.get(playerSymbol);
-      winner.incrementScore();
-    } else if (countingMoves === GRID_SIZE ** 2) {
-      let score = +drawScore.textContent;
-      drawScore.textContent = ++score;
-      gameOver = true;
-    }
-
+    isNextWinningMove(index, playerSymbol);
     findWinningMove(index, playerSymbol)
     nextTurn();
   }
 
-  return { processPlayerMove };
-})(playerOne, playerTwo);
+  const resetScores = () => {
+    playerOne.getPlayerScore().textContent = 0;
+    playerOne.getPlayerScore().classList.remove("player-1-color");
+  
+    playerTwo.getPlayerScore().textContent = 0;
+    playerTwo.getPlayerScore().classList.remove("player-2-color");
+  
+    drawScore.textContent = 0;
+  }
+  
+  const resetBoard = () => {
+    Array.from(gridBoard.children).forEach(grid => grid.className = "");
+    playerOne.getPlayerScore().classList.remove("player-1-color");
+    playerTwo.getPlayerScore().classList.remove("player-2-color");
+    gameOver = false;
+    gridArray = Array(10);
+    nextWinningMove = new Map();
+    playerOneTurn = true;
+    countingMoves = 0;
+  }
+
+  return { processPlayerMove, resetScores, resetBoard  };
+  
+})(playerOne, playerTwo, drawScore);
 
 gridBoard.addEventListener("click", (event) => {
   event.preventDefault();
@@ -163,3 +183,5 @@ gridBoard.addEventListener("click", (event) => {
 
 playerOneInput.onchange = function() { playerOne.setPlayerName(this.value); };
 playerTwoInput.onchange = function() { playerTwo.setPlayerName(this.value); };
+resetBoardButton.onclick = function() { gameGrid.resetBoard(); };
+resetScoreButton.onclick = function() { gameGrid.resetScores(); };
